9749a6fcd333e78581294ee0b2eac760abad580a
Brezzor <oliv095w@edu.zealand.dk>
2024-06-07 22:48:22 +0200

    Changed vue router history type

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import ContactView from '../views/ContactView.vue'
@@ -6,7 +6,7 @@ import WorksView from '../views/WorksView.vue'
 import SkillsView from '@/views/SkillsView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
