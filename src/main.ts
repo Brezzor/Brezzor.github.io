423a3a93f0147fee5202d5f9766b979fc7abea66
Oliver Bresson <oliver.bresson@gmail.com>
2024-06-27 04:41:13 +0200

    test for new github actions

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+}
+
+initializeApp(firebaseConfig)
+
 const app = createApp(App)
 
 app.use(createPinia())
