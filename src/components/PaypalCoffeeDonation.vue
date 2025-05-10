f9c6a6bb6ce1eebf0548046f4958d5a504636a27
Brezzor <oliver.bresson@gmail.com>
2025-05-10 23:04:34 +0200

    Fixed localization for donation text and added the corresponding donation button.

--- src/components/PaypalCoffeeDonation.vue
+++ src/components/PaypalCoffeeDonation.vue
@@ -3,13 +3,19 @@
 
 <template>
     <div class="text-center">
-        <p>Køb mig en kop kaffe. Tak for din støtte!</p>
-        <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <p>{{ $t('DonateText') }}</p>
+        <form v-if="$i18n.locale === 'da'" action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="hosted_button_id" value="EWBC67WQU8XHA" />
-            <input type="image" src="https://www.paypalobjects.com/da_DK/i/btn/btn_donate_LG.gif" border="0"
+            <input type="image" src="https://www.paypalobjects.com/da_DK/DK/i/btn/btn_donateCC_LG.gif" border="0"
                 name="submit" title="PayPal - The safer, easier way to pay online!"
                 alt="Doner penge med PayPal-knappen" />
             <img alt="" border="0" src="https://www.paypal.com/da_DK/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+        <form v-else action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id" value="V8BVEH82UUZT6" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0"
+                name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_DK/i/scr/pixel.gif" width="1" height="1" />
         </form>
     </div>
 </template>
